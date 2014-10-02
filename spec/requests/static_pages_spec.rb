09b4114e5607245fee16dd6291b44e5487a5cb11
anastasiosl <anastasiosl@thinknear.com>
2014-10-02 12:43:52 -0700

    Finished Static Pages chapter and excercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   # Rspec test for the home page
   describe "Home page" do
@@ -14,7 +16,7 @@ describe "Static pages" do
     #Check the title
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -29,7 +31,7 @@ describe "Static pages" do
     #Check the title
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -44,7 +46,22 @@ describe "Static pages" do
     #Check the title
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('About Us')
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  
+  #Rspec test for the contact us page
+  describe "Contact page" do
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    # Check the title
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
   
